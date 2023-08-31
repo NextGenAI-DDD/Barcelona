8514d6056442f4975582936c50578d2cb8ee3248
Adrian <adrian.kemski@viamedica.pl>
2023-08-31 08:09:50 +0200

    Added migrations, storage and photos

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -97,6 +97,17 @@
                                             </div>
                                         </div>
 
+                                        <div class="row mb-3">
+                                            <label for="photo" class="col-md-4 col-form-label text-md-end">{{ __('Photo') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="photo" type="file" class="form-control" name="photo">
+
+                                            </div>
+                                        </div>
+
+
+
                                         <div class="row mb-0">
                                             <div class="col-md-6 offset-md-4">
                                                 <button type="submit" class="btn btn-primary">
