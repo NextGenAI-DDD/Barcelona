ff2b2fc273553eb5785a2d28a2d0c09250003f03
Adrian <kemski12321@gmail.com>
2023-08-31 17:11:18 +0100

    zmainy

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('surName');
             $table->string('przydomek');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at');
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
